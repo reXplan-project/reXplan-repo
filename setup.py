47da2339d8e4a88ec0d14970707556618a90b418
Pablo Gallegos <pablo.gallegos@tractebel.engie.com>
2022-08-25 14:00:15 +0200

    added requirements.txt and modified setup.py version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="resiliencyTool",
-    version="0.0.1",
+    version="0.0.2",
     packages=['resiliencyTool'],
     zip_safe=False,
     author="ENGIE impact",
